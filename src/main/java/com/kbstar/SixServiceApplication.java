3cf9dbf1a0224f66e496e12eea4ef5cadd57b7b2
eejeong <wdwaaa7325@gmail.com>
2023-05-11 15:25:57 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/SixServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixServiceApplication.class, args);
+    }
+
+}
